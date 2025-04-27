6734fe854dfd4584c697408c10581993e9d16336
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-04-27 21:20:40 +0300

    patch 0.1.12 turned off currentColor-property in webpack config for project & storybook

--- .storybook/webpack.config.ts
+++ .storybook/webpack.config.ts
@@ -1,50 +1,51 @@
-import webpack from 'webpack';
-import path from 'path';
+import webpack from 'webpack'
+import path from 'path'
 
-import { BuildPaths } from '../config/build/types/config';
-import { buildCssLoaders } from '../config/build/loaders/buildCssLoaders';
+import { BuildPaths } from '../config/build/types/config'
+import { buildCssLoaders } from '../config/build/loaders/buildCssLoaders'
 
-export default ({ config }: {config: webpack.Configuration }) => {
-	const paths: BuildPaths = {
+export default ({ config }: { config: webpack.Configuration }) => {
+    const paths: BuildPaths = {
         entry: '',
         build: '',
-        src: path.resolve(__dirname, '..', '..', 'src'),
-    };
-    config.resolve?.modules?.push(paths.src);
-    config.resolve?.extensions?.push('.ts', '.tsx');
+        src: path.resolve(__dirname, '..', '..', 'src')
+    }
+    config.resolve?.modules?.push(paths.src)
+    config.resolve?.extensions?.push('.ts', '.tsx')
 
     if (config.module?.rules) {
-         
-        config.module.rules = config?.module?.rules?.map((rule) => {
+        config.module.rules = config?.module?.rules?.map(rule => {
             if (rule && typeof rule !== 'string') {
                 if (/svg/.test(rule.test as string)) {
-                    return { ...rule, exclude: /\.svg$/i };
+                    return { ...rule, exclude: /\.svg$/i }
                 }
             }
-            return rule;
-        });
+            return rule
+        })
     }
 
     config.module?.rules?.push({
         test: /\.svg$/,
-        use: [{
-            loader: '@svgr/webpack',
-            options: {
-                icon: true,
-                svgoConfig: {
-                    plugins: [
-                        {
-                            name: 'convertColors',
-                            params: {
-                                currentColor: true,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    //currentColor: true,
+                                }
                             }
-                        }
-                    ]
+                        ]
+                    }
                 }
             }
-        }],
-    });
+        ]
+    })
 
-    config.module?.rules?.push(buildCssLoaders(true));
-	return config;
+    config.module?.rules?.push(buildCssLoaders(true))
+    return config
 }
--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,50 +1,47 @@
-import webpack from 'webpack';
-import { buildCssLoaders } from './loaders/buildCssLoaders';
-import { BuildOptions } from './types/config';
+import webpack from 'webpack'
+import { buildCssLoaders } from './loaders/buildCssLoaders'
+import { BuildOptions } from './types/config'
 
 export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
-    const {isDev} = options;
+    const { isDev } = options
 
     const svgLoader = {
         test: /\.svg$/,
-        use: [{
-            loader: '@svgr/webpack',
-            options: {
-                icon: true,
-                svgoConfig: {
-                    plugins: [
-                        {
-                            name: 'convertColors',
-                            params: {
-                                currentColor: true,
+        use: [
+            {
+                loader: '@svgr/webpack',
+                options: {
+                    icon: true,
+                    svgoConfig: {
+                        plugins: [
+                            {
+                                name: 'convertColors',
+                                params: {
+                                    //currentColor: true
+                                }
                             }
-                        }
-                    ]
+                        ]
+                    }
                 }
             }
-        }],
-    };
+        ]
+    }
 
     const fileLoader = {
         test: /\.(png|jpe?g|gif|ttf)$/i,
         use: [
             {
-                loader: 'file-loader',
-            },
-        ],   
-    };
-    const cssLoader = buildCssLoaders(isDev);
+                loader: 'file-loader'
+            }
+        ]
+    }
+    const cssLoader = buildCssLoaders(isDev)
 
     const typescriptLoader = {
         test: /\.tsx?$/,
         use: 'ts-loader',
-        exclude: /node_modules/,
-    };
+        exclude: /node_modules/
+    }
 
-    return [
-        fileLoader,
-        svgLoader,
-        cssLoader,
-        typescriptLoader,  
-    ]
+    return [fileLoader, svgLoader, cssLoader, typescriptLoader]
 }
