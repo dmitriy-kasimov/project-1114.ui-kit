173fb5f7693f01bfd41009a8ecfea15fe04aeaa3
Dmitriy K. <its.dmitriy.kasimov@gmail.com>
2025-05-30 16:29:26 +0300

    patch 0.1.23 fixed the bug with switchers and tooltip in popups

--- src/ui/test/Test.tsx
+++ src/ui/test/Test.tsx
@@ -1,16 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Modal } from 'ui/utils/Modal'
-import { VStack } from 'ui/components/shared/Stack'
+import { HStack, VStack } from 'ui/components/shared/Stack'
 import { Text } from 'ui/components/shared/Text'
 import { Tooltip } from 'ui/components/shared/Tooltip'
 import { Switcher } from 'ui/components/controls/inputs/Switcher'
-export const Test = () => {
-    return (
-        <Modal isOpen={true}>
-            <VStack gap={'s'}>
+import { ListBox, ListBoxItem } from 'ui/components/controls/popups'
+import { Skeleton } from 'ui/components/shared/Skeleton'
+
+const listBoxItems: ListBoxItem<string>[] = [
+    {
+        content: (
+            <HStack gap='s'>
+                <Skeleton height={30} width={30} border='50%' />
                 <Tooltip content={<Text>Hello World!</Text>}>
                     <Text>Hover on me</Text>
                 </Tooltip>
+            </HStack>
+        ),
+        value: 'M1RoN'
+    },
+    {
+        content: (
+            <HStack gap='s'>
+                <Skeleton height={30} width={30} border='50%' />
+                <Tooltip content={<Text>Hello World!</Text>}>
+                    <Text>Hover on me</Text>
+                </Tooltip>
+            </HStack>
+        ),
+        value: 'Alan_Masti'
+    }
+]
+export const Test = () => {
+    const [listBoxValue, setListBoxValue] = useState('Alan_Masti')
+
+    return (
+        <Modal isOpen={true}>
+            <VStack gap={'s'}>
+                <ListBox
+                    value={listBoxValue}
+                    onChange={setListBoxValue}
+                    items={listBoxItems}
+                    defaultValue='Alan_Masti'
+                />
                 <Switcher id={'test'} value={true} onChange={() => {}} />
                 <Switcher id={'test2'} value={true} onChange={() => {}} />
             </VStack>
